7d7f704220f75901be397e9d352d0c1e6f9ffad7
xywang71 <xywang71@iflytek.com>
2020-12-24 17:06:42 +0800

    feat: 添加mockData

---
+++ src/mock/index.js
@@ -0,0 +1,30 @@
+export const mockData = [
+    {
+        id: 1,
+        title: 'iPhone 12 Pro',
+        price: 9999,
+        img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1825215635,1500412764&fm=11&gp=0.jpg',
+        amount: 1,
+    },
+    {
+        id: 2,
+        title: 'iPhone 11 Pro',
+        price: 7999,
+        img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.9pai.net%2Fupload%2F62d6e1fa-a792-4a09-bedd-2b60f7a1e36f.JPG&refer=http%3A%2F%2Fwww.9pai.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611392519&t=79abd8d30fa8a0aff92b3d7d3ec64c7c',
+        amount: 1,
+    },
+    {
+        id: 3,
+        title: 'Huawei Mate40 Pro',
+        price: 6999,
+        img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.zhimg.com%2Fv2-28c12fdb31c26c9aa3ddd13b0cefb4e1_250x0.jpg%3Fsource%3D172ae18b&refer=http%3A%2F%2Fpic1.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611392646&t=bd2fbde8fce42d6fbd8e53e668e4d5e1',
+        amount: 1,
+    },
+    {
+        id: 4,
+        title: 'Xiaomi 10 Pro',
+        price: 4999,
+        img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.yeedee.com.cn%2Fupload%2F41a4be79-19ff-49d8-bc2a-9b3d3adc6bae.JPG&refer=http%3A%2F%2Fwww.yeedee.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611392721&t=e9013f7379566b1530046b25dc64b753',
+        amount: 1,
+    },
+] 
